c71e2d912f76e71407a6a26334dbc4a6b9145c9c
Isanin Sergey <sergey.isanin@gmail.com>
2024-11-04 17:42:07 +0300

    feat(analytics): добавлен плагин для подключения Яндекс.Метрики
    - создан плагин yandex-metrika.client.js для инициализации Яндекс.Метрики на клиентской стороне;
    - добавлена проверка на client-only исполнение для корректной работы на Nuxt 3;
    - в app.vue добавлен <noscript> блок для отслеживания пользователей с отключённым JavaScript.

--- app.vue
+++ app.vue
@@ -1,4 +1,7 @@
 <template>
   <NuxtPage />
   <NuxtRouteAnnouncer />
+  <noscript>
+    <div><img src="https://mc.yandex.ru/watch/98816322" style="position:absolute; left:-9999px;" alt="" /></div>
+  </noscript>
 </template>
