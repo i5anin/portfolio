5d3573d698d0b0d388181c5e7869e41c7a4c2d17
Isanin Sergey <sergey.isanin@gmail.com>
2024-11-06 22:50:42 +0300

    enh(ui): добавлена иконка GitHub и улучшено оформление RepositoryCard
    - Добавлена иконка GitHub рядом с названием репозитория в компоненте RepositoryCard
    - Применены стили Bootstrap для выравнивания иконки и текста
    - Использован класс `d-flex` и `align-items-center` для правильного выравнивания
    - Оптимизирована структура шаблона для улучшения восприятия интерфейса

--- components/RepositoryCard.vue
+++ components/RepositoryCard.vue
@@ -1,16 +1,33 @@
 <template>
   <div class="repository-card">
-    <h2><span class="badge text-bg-secondary text-bg-success">{{ repository.name }}</span></h2>
+    <!-- Иконка GitHub и название репозитория -->
+    <div class="d-flex align-items-center mb-2">
+
+      <h2>
+        <span class="badge text-bg-secondary text-bg-success">
+          {{ repository.name }}
+        </span>
+      </h2>
+      <SvgIcon type="mdi" :path="mdiGithub" class="github-icon ms-2" />
+    </div>
+
+    <!-- Количество коммитов -->
     <p>Количество коммитов:
-      <span v-if="repository.commitCount !== null"><b>{{ repository.commitCount }}</b></span>
-      <span v-else class="placeholder-glow"><span class="placeholder col-3"></span></span>
+      <span v-if="repository.commitCount !== null">
+        <b>{{ repository.commitCount }}</b>
+      </span>
+      <span v-else class="placeholder-glow">
+        <span class="placeholder col-3"></span>
+      </span>
     </p>
 
+    <!-- Дата последнего коммита и количество дней с момента последнего коммита -->
     <p>Дата последнего коммита:
       <b>{{ latestCommitDate || 'Неизвестно' }}</b>
       <span v-if="daysSinceLastCommit !== null"> ({{ daysSinceLastCommit }} дней назад)</span>
     </p>
 
+    <!-- Сообщение последнего коммита -->
     <div v-if="repository.latestCommit">
       <p><strong>Последний коммит:</strong></p>
       <pre class="no-scroll">Сообщение: {{ repository.latestCommit.message }}</pre>
@@ -19,12 +36,21 @@
 </template>
 
 <script>
+import SvgIcon from '@jamescoyle/vue-icon';
+import { mdiGithub } from '@mdi/js';
+
 export default {
+  components: { SvgIcon },
   props: {
     repository: {
       type: Object,
       required: true,
     },
+  },
+  data() {
+    return {
+      mdiGithub,
+    };
   },
   computed: {
     latestCommitDate() {
@@ -52,6 +78,13 @@ export default {
   border-radius: 8px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
+
+.github-icon {
+  width: 24px;
+  height: 24px;
+  color: #333; /* Цвет иконки GitHub */
+}
+
 .no-scroll {
   white-space: pre-wrap; /* Позволяет тексту переноситься */
   overflow-wrap: break-word; /* Разбивает длинные слова */
